1d113d31eb0dd2eb156b464edc68450a2eb611f5
ekapratama363 <ekapratama363@gmail.com>
2022-06-16 11:03:27 +0700

    cahnge title

--- application/views/admin_panel/pages/auth/login.php
+++ application/views/admin_panel/pages/auth/login.php
@@ -3,12 +3,12 @@
 <head>
 
         <meta charset="utf-8" />
-        <title>Login | Pancanature</title>
+        <title>Login | Inbi</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
         <meta content="Themesbrand" name="author" />
         <!-- App favicon -->
-        <link rel="shortcut icon" href="<?php echo base_url(); ?>assets/frontend/images/pancanature.png">
+        <link rel="shortcut icon" href="<?php echo base_url(); ?>assets/images/fav-icon.png">
         
         <!-- Bootstrap Css -->
         <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
